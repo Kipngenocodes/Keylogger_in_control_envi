b030eef69d3427d490118d43de2f4a955ddaa1bc
Kipngenocodes <kipngenoalex.c@gmail.com>
2024-08-25 14:40:50 -0700

    file integrity management system and sample text added

---
+++ File_integrity_management.py
@@ -0,0 +1,11 @@
+# the purpose is build a file integrity management for sample text 
+
+# import libraries that wil interact with your files
+import os
+import hashlib
+import json
+import time
+
+
+
+
