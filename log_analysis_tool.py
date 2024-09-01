d5be2e88be4934f2248f1559717bb245e0e9f5e6
Kipngenocodes <kipngenoalex.c@gmail.com>
2024-09-01 02:48:29 -0700

    parse ,og entry added

--- log_analysis_tool.py
+++ log_analysis_tool.py
@@ -19,4 +19,20 @@ class log_analysi:
     def loading_logs(self):
         # loading log data from a file into a memory
         with open(self.log_file, 'r') as file:
+           self.log_data = file.readlines()
+           print(f'loaded {len(self.log_data)} log entries')
            
+    def parsing_log_entry(self, log_entry):
+        # parsing log entry into a dictionary
+        #  a regular expression to extract the timestamp, log level,  and message.
+        log_pattern = re.compile(r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) (\w+) (.*)')
+        if matching := log_pattern.match(log_entry):
+            return {
+                'timestamp': datetime.stripetime(matching[1], "%Y-%m-%d %H:%M:%S"),
+                'log_level': matching[2],
+                'message': matching[3],
+            }
+            
+        else:
+            return None
+        
